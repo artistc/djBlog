a24a013f516941cd9f8018d7aa63d9aae9ed7486
Azamat Galimzhanov <galimzhanov.azamat@yahoo.com>
2011-11-22 02:37:32 +0000

    Archive + categories implemented.

--- blog/urls.py
+++ blog/urls.py
@@ -3,21 +3,29 @@ from django.conf.urls.defaults import *
 
 # Views
 from django.views.generic import list_detail
+from blog.views import *
+from django.views.generic import list_detail, date_based
 
 # Models
 from blog.models import Post
 
 urlpatterns = patterns('',
-    url(r'post/(?P<slug>[a-z-]+)/$',
-             list_detail.object_detail,
-                 {'queryset': Post.objects.all(),
-                  'template_object_name': 'post',
-                  'slug_field' : 'slug',},
-                  name='single_post'),
+    # Post detail
+    url(r'post/(?P<slug>[a-z-]+)/$', blog_generic_view,
+        {'redirect_to': list_detail.object_detail, 'slug_field' : 'slug',},
+        name='single_post'),
 
-    # Homepage is base template with nothing else (i.e. index page)
-    url(r'^$', list_detail.object_list,
-        {'queryset': Post.objects.all(),
-        'template_object_name': 'post',},
+    # Posts by category
+    url(r'^category/(\d+)$', blog_posts_by_category,
+        name='blog_posts_by_category'),
+
+    # Archives (posts by date)
+    url(r'^archive/(?P<month>[a-z]+)/(?P<year>\d{4})/$', blog_generic_view,
+        {'redirect_to': date_based.archive_month, 'date_field': 'published',
+        'template_name': 'blog/post_list.html',},
+        name='blog_posts_by_month'),
+
+    # Home page (post list)
+    url(r'^$', blog_generic_view, {'redirect_to': list_detail.object_list},
         name="blog_home"),
 )
