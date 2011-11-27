9288d94da14e1a3f524cdd36d7a64efdb605a290
Azamat Galimzhanov <galimzhanov.azamat@yahoo.com>
2011-11-27 16:53:08 +0000

    Post formatting in admin.

--- blog/admin.py
+++ blog/admin.py
@@ -6,5 +6,14 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'published')
     prepopulated_fields = {'slug' : ('title',)}
 
+    class Media:
+        js = (
+              'https://ajax.googleapis.com/ajax/libs/dojo/1.6.0/dojo/dojo.xd.js',
+              '/static/admin/js/editor.js',
+             )
+        css = {
+               'all': ('/static/admin/css/editor.css',),
+              }
+
 admin.site.register(Category)
 admin.site.register(Post, PostAdmin)
